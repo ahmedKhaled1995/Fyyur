8190fe5f24c50de87e9edd46da5c0893b43cdffa
unknown <bigbossak84@gmail.com>
2020-09-20 21:15:27 +0200

    Added Migrations

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -21,7 +23,8 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
+# TODO: connect to a local postgresql database #Done
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,6 @@ window.parseISOString = function parseISOString(s) {
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
 
-console.log("Script loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page loaded");
+});
